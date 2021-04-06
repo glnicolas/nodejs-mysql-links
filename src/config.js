8e173bd1ba45d5a034a24b0370e52c2ac9593beb
Nicolas Garcia Lira <nico17.ngl@gmail.com>
2021-04-06 01:05:51 -0500

    Updated config

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
     connectionLimit: 10,
     host: process.env.DATABASE_HOST || "localhost",
     user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "faztpassword",
+    password: process.env.DATABASE_PASSWORD || "clan.3cp",
     database: process.env.DATABASE_NAME || "dblinks",
   },
   port: process.env.PORT || 4000,
